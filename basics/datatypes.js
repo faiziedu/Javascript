808d26dabd3c98dacef45bb2571e76f39f41a31c
faiziedu <124394230+faiziedu@users.noreply.github.com>
2024-02-06 18:17:44 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) //we are using node js, not browser
+
+let name="Faizan"
+let age=20
+let isLoggedIn=false
+let state;
+
+// number=> 2^53
+// bigint
+// string=> ""
+// boolean=> ture/false
+// null => standalone value
+// undefined 
+// symbol => for unique
+// object
+
+console.log(typeof "hi");
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId=1234
+let accountEmail="faizan123@gmail.com"
+var accountPassword="4321"
+accountCity="Lahore"
+let accountState
+
+// accountId=2
+console.log(accountId);
+accountEmail="faiz321@gmail.com"
+accountPassword="1234"
+accountCity="Hujra"
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
